7d6263aa5a86f8b60e7a74b64eb4b3538a0f1e3a
Matteo Brancaleoni <mbrancaleoni@gmail.com>
2019-06-09 11:20:09 +0200

    show time on default screen

--- lib/detectino_panel/scene/default.ex
+++ lib/detectino_panel/scene/default.ex
@@ -2,8 +2,11 @@ defmodule DetectinoPanel.Scene.Default do
   @moduledoc false
   use Scenic.Scene
 
+  alias Detectino.Api.Events
   alias DetectinoPanel.Components, as: MyComponents
   alias Scenic.{Graph, ViewPort}
+
+  import Scenic.Primitives, only: [{:text, 3}]
 
   require Logger
 
@@ -28,6 +31,8 @@ defmodule DetectinoPanel.Scene.Default do
           |> MyComponents.blank([], id: :blank)
       end
 
+    Events.subscribe(:timer)
+
     {:ok, %State{graph: g, blanked: blanked?}, push: g}
   end
 
@@ -36,16 +41,37 @@ defmodule DetectinoPanel.Scene.Default do
       Graph.build()
       |> MyComponents.blank([], id: :blank)
 
+    {:noreply, %{state | graph: g, blanked: true}, push: g}
+  end
+
+  def handle_info({:timer, _}, %{blanked: true} = state) do
+    {:noreply, state}
+  end
+
+  def handle_info({:timer, current_dt}, %{blanked: false} = state) do
+    g =
+      @graph
+      |> text(format_dt(current_dt),
+        fill: :antique_white,
+        text_align: :center_middle,
+        font_size: 32,
+        translate: {400, 200}
+      )
+
     {:noreply, %{state | graph: g}, push: g}
   end
 
   def filter_event({:blank_click}, _, state) do
-    {:halt, %{state | graph: @graph}, push: @graph}
+    {:halt, %{state | graph: @graph, blanked: false}, push: @graph}
   end
 
   def filter_event({:background_click}, _, state) do
     ViewPort.set_root(:main_viewport, {DetectinoPanel.Scene.Main, nil})
 
     {:halt, state}
+  end
+
+  defp format_dt(dt) do
+    Timex.format!(dt, " %H:%M:%S\n%A, %B %-d %Y", :strftime)
   end
 end
